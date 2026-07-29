af77273e4d0bd8eabf1f0b842f7d691edc83e18d
adam <adithjohn.ipcs@gmail.com>
2026-07-29 18:12:40 +0530

    Added Chapter 01 - Basics programs

--- Chapter-01_Basics/hello.c
+++ Chapter-01_Basics/hello.c
@@ -1,9 +1,13 @@
+//Author - Adhith D John
+
+
 #include <stdio.h>
 
 int main()
 {
     printf("Hello, World!\n");
     printf("Welcome to C Programming.\n");
+    printf("IPCS  GLOBAL - World's best Technical Training Provider");
 
     return 0;
-}[A[A[A[A[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C present by IPCS GLOBAL)[D");
+}
